fffe54270b619da0bb4cc719f6fb8a222d0b8522
Ashley Sharpe <ashleysharpe@MacBook-Pro-10.local>
2021-11-05 12:45:17 -0400

    calculation functionality part1

--- cal.js
+++ cal.js
@@ -3,3 +3,13 @@ let result = document.getElementById("inputext");
 let calculate=(number)=>{
     result.value+=number;
 }
+
+let Result=()=>{
+    try{
+        result.value=eval(result.value)
+    }
+    catch(err){
+        alert("Enter a valid result");
+    }
+}
+
